591f05711f66ed63dd2a0ae0277a69d63897284e
Tiago Castro <tiagocastro602@gmail.com>
2022-12-17 12:17:41 -0300

    save notification

--- src/infra/database/prisma/repositories/notifications.ts
+++ src/infra/database/prisma/repositories/notifications.ts
@@ -43,6 +43,13 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
   }
 
   async save(notification: Notification): Promise<void> {
-    throw new Error('Method not implemented.');
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notification.update({
+      where: {
+        id: raw.id,
+      },
+      data: raw,
+    });
   }
 }
